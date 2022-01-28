429f8f450d7c4b88741d22075f5914bb5a848ef3
Jaap Frolich <jfrolich@gmail.com>
2022-01-28 19:35:38 +0800

    Tests improvements

--- tests.sh
+++ tests.sh
@@ -18,7 +18,7 @@ taskCount=0
 function maybeWait {
   let taskCount+=1
   # spawn in batch of 20 processes
-  [[ $((taskCount % 20)) = 0 ]] && wait
+  [[ $((taskCount % 20)) = 0 ]] && printf "." && wait
 }
 
 platform='darwin'
@@ -26,6 +26,9 @@ refmt_path="./node_modules/rescript/${platform}/refmt.exe"
 ppx_path="./_build/default/src/bucklescript_bin/bin.exe"
 bsb_path="./node_modules/rescript/${platform}/bsc.exe"
 configs=('records' 'template' 'apollo' 'native')
+
+rm -rf tests_bucklescript/operations/expected/
+rm -rf tests_bucklescript/operations/error/expected/
 
 for config in $configs; do
   case $config in
@@ -36,13 +39,16 @@ for config in $configs; do
   esac
   mkdir -p tests_bucklescript/operations/expected/$config/generate
   mkdir -p tests_bucklescript/operations/expected/$config/compile
+  mkdir -p tests_bucklescript/operations/errors/expected
   for file in tests_bucklescript/operations/*.re; do
     $refmt_path --parse=re --print=binary $file | $ppx_path -schema=graphql_schema.json $opts /dev/stdin /dev/stdout | $refmt_path --parse=binary &> $(snapshotGeneratePath $file $config) & maybeWait
 
     if [[ $config != "native" ]]; then
-      # $bsb_path -schema=graphql_schema.json $opts $file &> $(snapshotCompilePath $file $config) & maybeWait
       $bsb_path -I ./utilities -w -30 -ppx "$ppx_path -schema=graphql_schema.json $opts" $file &> $(snapshotCompilePath $file $config) & maybeWait
     fi
+  done
+  for file in tests_bucklescript/operations/errors/*.re; do
+    $bsb_path -I ./utilities -w -30 -ppx "$ppx_path -schema=graphql_schema.json $opts" $file 2> $(snapshotErrorPath $file $config) 1> /dev/null & maybeWait
   done
 done
 
@@ -53,9 +59,10 @@ successGreen='\033[0;32m'
 reset='\033[0m'
 
 diff=$(git ls-files --modified tests_bucklescript/operations/**/expected)
+printf "\033[2K\r"
 if [[ $diff = "" ]]; then
   printf "${successGreen}✅ No unstaged tests difference.${reset}\n"
 else
-  printf "${warningYellow}⚠️ There are unstaged differences in tests/! Did you break a test?\n${diff}\n${reset}"
+  printf "${warningYellow}⚠️ There are unstaged differences in tests! Did you break a test?\n${diff}\n${reset}"
   exit 1
 fi
